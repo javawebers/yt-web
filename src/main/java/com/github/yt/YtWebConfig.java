6885431521ea2c90f90768e2993596960e0c2f51
limiao <18640290294@163.com>
2018-11-29 17:50:51 +0800

    yt-web
    yt-web

---
+++ src/main/java/com/github/yt/YtWebConfig.java
@@ -0,0 +1,75 @@
+package com.github.yt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class YtWebConfig {
+    public static String baseEntityValueClass;
+    public static boolean packageResponseBody;
+    public static String resultClass;
+
+    // 分页
+    public static String pageNoName;
+    public static String pageSizeName;
+    public static String pageTotalCountName;
+    public static String pageDataName;
+
+    // swagger
+    public static boolean showSwagger;
+
+    // 请求日志
+    // 是否记录请求日志
+    public static boolean requestLog;
+    // 如果记录日志是否记录post等的body内容，记录body内容在单元测试时会有问题，取不到body的值
+    public static boolean requestLogBody;
+
+    @Value("${yt.swagger.show:false}")
+    public void setShowSwagger(boolean showSwagger) {
+        YtWebConfig.showSwagger = showSwagger;
+    }
+
+    @Value("${yt.entity.baseEntityValue:com.github.yt.mybatis.domain.DefaultBaseEntityValue}")
+    public void setBaseEntityValueClass(String baseEntityValueClass) {
+        YtWebConfig.baseEntityValueClass = baseEntityValueClass;
+    }
+
+    @Value("${yt.result.packageResponseBody:true}")
+    public void setPackageResponseBody(boolean packageResponseBody) {
+        YtWebConfig.packageResponseBody = packageResponseBody;
+    }
+
+    @Value("${yt.result.class:com.github.yt.web.result.SimpleResultConfig}")
+    public void setResultClass(String resultClass) {
+        YtWebConfig.resultClass = resultClass;
+    }
+
+    @Value("${yt.page.pageNoName:pageNo}")
+    public void setPageNoName(String pageNoName) {
+        YtWebConfig.pageNoName = pageNoName;
+    }
+
+    @Value("${yt.page.pageSizeName:pageSize}")
+    public void setPageSizeName(String pageSizeName) {
+        YtWebConfig.pageSizeName = pageSizeName;
+    }
+
+    @Value("${yt.page.pageTotalCountName:totalCount}")
+    public void setPageTotalCountName(String pageTotalCountName) {
+        YtWebConfig.pageTotalCountName = pageTotalCountName;
+    }
+
+    @Value("${yt.page.pageDataName:data}")
+    public void setPageDataName(String pageDataName) {
+        YtWebConfig.pageDataName = pageDataName;
+    }
+    @Value("${yt.request.requestLog:true}")
+    public void setRequestLog(boolean requestLog) {
+        YtWebConfig.requestLog = requestLog;
+    }
+    @Value("${yt.request.requestLogBody:false}")
+    public void setRequestLogBody(boolean requestLogBody) {
+        YtWebConfig.requestLogBody = requestLogBody;
+    }
+}
+
