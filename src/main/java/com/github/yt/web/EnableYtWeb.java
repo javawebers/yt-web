f5b2576af5843d4ac547044447e616c1120c05f6
likeabook <likeabook@163.com>
2018-12-06 20:26:19 +0800

    分页增强支持单独使用

--- src/main/java/com/github/yt/web/EnableYtWeb.java
+++ src/main/java/com/github/yt/web/EnableYtWeb.java
@@ -11,6 +11,13 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
+
+/**
+ * 1.开启分页参数传入功能
+ * 2.返回体自动包装，包括无异常返回和异常返回两种情况的处理
+ * 3.记录请求信息
+ * @author liujiasheng
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
