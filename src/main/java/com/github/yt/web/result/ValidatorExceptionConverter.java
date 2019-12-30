97e0b05a95f7ebe749ec11e883a2878509eecb04
likeabook <likeabook@163.com>
2019-12-30 15:45:30 +0800

    代码格式化

--- src/main/java/com/github/yt/web/result/ValidatorExceptionConverter.java
+++ src/main/java/com/github/yt/web/result/ValidatorExceptionConverter.java
@@ -32,26 +32,26 @@ public class ValidatorExceptionConverter implements BaseExceptionConverter {
             ConstraintViolationException cve = (ConstraintViolationException)e;
             Set<ConstraintViolation<?>> cvSet = cve.getConstraintViolations();
             List<String> errorMessageList = cvSet.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
-            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, YtStringUtils.join(errorMessageList, ","));
+            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, YtStringUtils.join(errorMessageList, ", "));
         }  else if (e instanceof BindException) {
             // controller校验异常拦截，类参数异常拦截
             BindException be = (BindException)e;
             List<ObjectError> errorList = be.getAllErrors();
             List<String> errorMessageList = errorList.stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
-            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, YtStringUtils.join(errorMessageList, ","));
+            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, YtStringUtils.join(errorMessageList, ", "));
         }  else if (e instanceof MethodArgumentNotValidException) {
             // controller校验异常拦截，RequestBody校验失败
             MethodArgumentNotValidException manve = (MethodArgumentNotValidException)e;
             List<ObjectError> errorList = manve.getBindingResult().getAllErrors();
             List<String> errorMessageList = errorList.stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
-            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, YtStringUtils.join(errorMessageList, ","));
+            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, YtStringUtils.join(errorMessageList, ", "));
         }  else if (e instanceof HttpMessageNotReadableException) {
             // controller校验异常拦截，RequestBody json转换异常，还没到校验步骤
             return new BaseAccidentException(YtWebExceptionEnum.CODE_12, e, e.getMessage());
         }  else if (e instanceof MethodArgumentTypeMismatchException) {
             // 參數異常，RequestBody
             MethodArgumentTypeMismatchException se = (MethodArgumentTypeMismatchException)e;
-            return new BaseAccidentException(YtWebExceptionEnum.CODE_11,  e, "参数:" + se.getName() + ",值:" + se.getValue());
+            return new BaseAccidentException(YtWebExceptionEnum.CODE_11, e, "参数:" + se.getName() + ", 值:" + se.getValue());
         }
         return e;
     }
